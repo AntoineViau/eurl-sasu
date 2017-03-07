c665e11f956655e604030fedd8eb267f54c1e52e
Antoine Viau <contact@antoineviau.com>
2017-03-07 18:01:56 +0100

    Nouveau design, url pour partage

--- app.ts
+++ app.ts
@@ -21,7 +21,8 @@ class AppCtrl {
     public resteJoursParSemaine: number;
     public result;
     public newStateName: string;
- 
+    public url;
+
     public states: Array<{ name: string, params: any }> = [];
     public currentState: any;
 
@@ -29,7 +30,8 @@ class AppCtrl {
         private $uibModal,
         private $sce,
         private exercice: Exercice,
-        private $cookies) {
+        private $cookies,
+        private $location) {
         this.params = {
             capital: { name: 'capital', min: 0, max: 100000, step: 100, value: 0 },
             charges: { name: 'charges', min: 0, max: 100000, step: 1000, value: 0 },
@@ -49,16 +51,18 @@ class AppCtrl {
             this.loadState();
         }
 
+        Object.keys(this.params).forEach(attr => this.params[attr].value = parseInt(this.$location.search()[attr]) || 0);
+
         // this.params['capital'].value = 2000;
         // this.params['charges'].value = 10000;
         // this.params['tj'].value = 500;
-        // this.params['nbJours'].value = 180;
-        // this.params['accre'].value = true;
-        // this.params['sa'].value = true;
-        // this.params['remuneration'].value = 28000;
-        // this.params['dividendes'].value = 39000;
-        // this.params['autresRevenus'].value = 9000;
-        // this.onChange();
+        // this.params['nbJours'].value = 200;
+        // this.params['accre'].value = false;
+        // this.params['sa'].value = false;
+        // this.params['remuneration'].value = 87000;
+        // this.params['dividendes'].value = 0;
+        // this.params['autresRevenus'].value = 0;
+        this.onChange();
     }
 
     pushState() {
@@ -75,20 +79,22 @@ class AppCtrl {
     }
 
     clearStates() {
-        if ( !confirm('Certain ?')) {
+        if (!confirm('Certain ?')) {
             return;
         }
         this.states = new Array<any>();
         this.$cookies.put(this.states, '[]');
     }
 
-    onChange() {
+    onChange(param = undefined) {
         let nbJours = this.params.nbJours.value;
         this.nbMois = Math.floor(nbJours / 21);
         this.resteJoursParMois = nbJours % 21;
         this.resteSemainesParMois = Math.floor(this.resteJoursParMois / 5);
         this.resteJoursParSemaine = this.resteJoursParMois % 5;
-
+        // if (param && param.name === 'remuneration') {
+        //     this.params.dividendes.value = 0;
+        // }
         this.exercice.capital = this.params.capital.value;
         this.exercice.ca = this.params.tj.value * this.params.nbJours.value;
         this.exercice.charges = this.params.charges.value;
@@ -98,10 +104,18 @@ class AppCtrl {
         this.exercice.autresRevenus = this.params.autresRevenus.value
         this.exercice.sa = this.params.sa.value;
         this.result = this.exercice.exercice();
+        // if (param && param.name === 'remuneration') {
+        //     this.params.dividendes.value = this.result.societe.reste;
+        //     this.onChange();
+        // }
+        this.url = this.$location.absUrl() + '/?' + Object.keys(this.params).map(k => k + '=' + this.params[k].value).join('&');
     }
 }
 
 angular.module('app', ['ngLocale', 'ui.bootstrap', 'ngSanitize', 'ngCookies'])
+    .config(['$locationProvider', ($locationProvider) => {
+        $locationProvider.html5Mode(true);
+    }])
     .service('cotisationsSociales', [() => new CotisationsSociales()])
     .service('impotSociete', [() => new ImpotSociete()])
     .service('impotRevenu', [() => new ImpotRevenu()])
@@ -110,8 +124,8 @@ angular.module('app', ['ngLocale', 'ui.bootstrap', 'ngSanitize', 'ngCookies'])
         (impotSociete, cotisationsSociales, impotRevenu) => new Exercice(impotSociete, cotisationsSociales, impotRevenu)
     ])
     .controller('appCtrl', [
-        '$uibModal', '$sce', 'exercice', '$cookies',
-        ($uibModal, $sce, exercice, $cookies) => new AppCtrl($uibModal, $sce, exercice, $cookies)
+        '$uibModal', '$sce', 'exercice', '$cookies', '$location',
+        ($uibModal, $sce, exercice, $cookies, $location) => new AppCtrl($uibModal, $sce, exercice, $cookies, $location)
     ])
     .component('field', {
         bindings: {
@@ -136,4 +150,19 @@ angular.module('app', ['ngLocale', 'ui.bootstrap', 'ngSanitize', 'ngCookies'])
                 });
             }
         }]
+    })
+    .directive('stringToNumber', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+                ngModel.$parsers.push(function (value) {
+                    //debugger;
+                    return parseInt(value);
+                });
+                ngModel.$formatters.push(function (value) {
+                    //debugger;
+                    return parseFloat(value);
+                });
+            }
+        };
     });
