ec5db3b6f83bcb93e5c316b53bdfca80a2dfda5a
Antoine Viau <contact@antoineviau.com>
2017-09-07 13:07:46 +0200

    Documentation

--- doc.ts
+++ doc.ts
@@ -26,7 +26,7 @@ De manière générale, l'idée de se rémunérer sur les dividendes signifie qu
     },
     assietteDividendesIR: {
         title: 'Assiette IR des dividendes',
-        content:`
+        content: `
 Un des gros avantages des dividendes est qu'ils sont taxés à l'IR sur 60% de leur valeur, et on peut aussi déduire la CSG de l'assiette (5,1%).  
 Exemple donné par [www.service-public.fr](https://www.service-public.fr/professionnels-entreprises/vosdroits/F32963) :  
 *Par exemple, un actionnaire ou associé qui perçoit 1 000 € de dividendes doit acquitter 15,5 % de prélèvements sociaux, soit 155 €, déductibles à hauteur de 5,1 %, soit 51 €. Le montant imposable des revenus distribués, soumis au barème progressif de l'impôt sur le revenu, est égal à 600 € (1 000 - 1 000 x 40 %) - 51 € (CSG déductible) = 549 €.*  
@@ -41,15 +41,22 @@ Par ailleurs, au moment où les dividendes sont distribués, il y a un acompte d
     },
     revenuBrut: {
         title: 'Revenu brut',
-        content: `Le revenu brut correspond à l'argent à destination personnelle avant tous les prélèvements.`
+        content: `Le revenu brut correspond à l'argent à destination personnelle avant tous les prélèvements.<br />
+        Par exemple : 100K de CA, 10K de charges, 20K d'ARE et on laisse 5K en société à la fin de l'exercice : <br />
+        100 - 10 + 20 - 5 = 105K de revenu brut perso.`
     },
     revenuNet: {
         title: 'Revenu net',
-        content: ``
+        content: `On parle en réalité de "super-net" : tous les prélèvements sont passés sur le brut, y compris l'impôt sur le revenu. Ce qui reste est donc vraiment ce qui va dans la poche du freelance.`
     },
     pourcentagePrelevements: {
         title: "Pourcentage de prélèvements",
-        content: ``
+        content: `Le pourcentage de prélèvement repose sur le revenu brut.<br />
+        Exemple : 100K de CA, 10K de charges, 5K reste en société, 30K de prélèvements (impôt sur le revenu et cotisations sociales).<br />
+        100 - 10 - 5 = 85K de revenu brut perso.<br />
+        85 - 30 = 55K de revenu net perso.<br />
+        Taux de prélèvement : 30 / 85 * 100 = 35,29 %
+        `
     }
 }
 
