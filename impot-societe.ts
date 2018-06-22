650fe272e479b77a0ab5f2f7af287d10cf3aeaa2
Guillaume Agoranet <team.dev@agoranet.fr>
2018-06-22 14:33:09 +0200

    ref AntoineViau/eurl-sasu#10 Nouvelles tranches d'IS

--- impot-societe.ts
+++ impot-societe.ts
@@ -1,26 +1,23 @@
 export default class ImpotSociete {
 
     public benefice: number;
-    public taux: number = 0.3333;
 
     getImpot(): number {
+        //En dessous de 38120, les bénéfices sont imposés à 15%
         if (this.benefice <= 38120) {
             return this.benefice * 0.15;
         }
         let is = 38120 * 0.15;
-        if (this.benefice <= 75000) {
+
+        //Entre 38120 et 500000, les bénéfices sont imposés à 28%
+        if (this.benefice <= 500000) {
             is += (this.benefice - 38120) * 0.28;
             return is;
         }
-        is += (75000 - 38120) * 0.28;
-        is += (this.benefice - 75000) * 0.33;
-        return is;
-    }
 
-    getTranches() {
-        let tranches = [];
-        tranches.push(this.benefice > 38120 ? 38120 * 0.15 : this.benefice * 0.15);
-        tranches.push(this.benefice < 38120 ? 0 : (this.benefice - 38120) * this.taux);
-        return tranches;
+        //Au delà de 500000, les bénéfices sont imposés à 33.33%
+        is += (this.benefice - 500000) * 0.33;
+
+        return is;
     }
 }
