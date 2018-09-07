5ecdcc167c342dff6d647495b0c5acea35cd37cf
Guillaume Agoranet <team.dev@agoranet.fr>
2018-09-07 17:25:57 +0200

    Add testUrl to JEST config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js"],
+  testURL: 'http://localhost/',
   testMatch: ["**/*.test.(ts|tsx|js)"],
   transform: {
     "^.+\\.ts?$": "ts-jest"
