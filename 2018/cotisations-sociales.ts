f7216373bb97e05064edb7ee05f02fb5b6b3db56
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-07-18 01:03:52 +0200

    RE-updated CS

--- 2018/cotisations-sociales.ts
+++ 2018/cotisations-sociales.ts
@@ -1,8 +1,9 @@
+// https://www.urssaf.fr/portail/home/taux-et-baremes/taux-de-cotisations/les-professions-liberales/bases-de-calcul-et-taux-des-coti.html
 export default class CotisationsSociales {
 
     remuneration: number = 0;
     accre: boolean;
-    PASS: number = 39228; // 2018 = 39732  
+    PASS: number = 39732;
 
     _revenuPro() {
         return this.remuneration;
@@ -53,20 +54,8 @@ export default class CotisationsSociales {
         return revenus * taux / 100;
     }
 
-    getAllocationsFamiliales() {
-        if (this._revenuPro() < 43705) {
-            return 0;
-        }
-        if (this._revenuPro() > 55625) {
-            return this._revenuPro() * 0.0310;
-        }
-        let pc = (this._revenuPro() - 43705) / (55625 - 43705);
-        let taux = pc * 3.1;
-        return this._revenuPro() * taux;
-    }
-
     getFormationProfessionnelle(): number {
-        return 38616 * 0.0025;
+        return this.PASS * 0.25 / 100;
     }
 
     getRetraiteBase(): number {
@@ -79,54 +68,72 @@ export default class CotisationsSociales {
         if (this._revenuPro() < 11.50 * this.PASS / 100) {
             montant = 461;
         }
-        if (this._revenuPro() < 4441) {
-            return 448;
+
+        // entre 4 569€ et 39 732€
+        if(this._revenuPro() >= 11.50 * this.PASS / 100 && this._revenuPro() < this.PASS) {
+            montant = 8.23 * this._revenuPro() / 100;
         }
-        let assiette = this._revenuPro();
-        if (assiette < this.PASS) {
-            return assiette * 0.0823 + assiette * 0.0187;
+
+        // de 39 732€ à 198 660€
+        if(this._revenuPro() >= this.PASS && this._revenuPro() < 5 * this.PASS) {
+            montant = (8.23 * this.PASS + 1.87 * this._revenuPro()) / 100;
         }
-        return this.PASS * 0.0823 + assiette * 0.0187;
+
+        // > à 198 660€
+        if(this._revenuPro() >= 5 * this.PASS) {
+            montant = (8.23 * this.PASS + 1.87 * 5 * this.PASS) / 100;
+        }
+
+        return montant;
     }
 
     getRetraiteComplementaire(): number {
-        if (this.accre && this._revenuPro() < 28962) {
-            return 0;
-        }
+        //https://www.lecoindesentrepreneurs.fr/affiliation-et-cotisations-a-la-cipav/
         let assiette = this._revenuPro();
-        let montant;
+
+        let montant = 0;
         if (assiette <= 26580) {
-            montant = 1277;
+            montant = 1315;
         }
         if (assiette > 26580 && assiette <= 49280) {
-            montant = 2553;
+            montant = 2630;
         }
         if (assiette > 49280 && assiette <= 57850) {
-            montant = 3830;
+            montant = 3945;
         }
         if (assiette > 57850 && assiette <= 66400) {
-            montant = 6384;
+            montant = 6575;
         }
         if (assiette > 66400 && assiette <= 83060) {
-            montant = 8937;
+            montant = 9205;
         }
         if (assiette > 83060 && assiette <= 103180) {
-            montant = 14044;
+            montant = 14465;
         }
         if (assiette > 103180 && assiette <= 123300) {
-            montant = 15320;
+            montant = 15780;
         }
         if (assiette > 123300) {
-            montant = 16597;
+            montant = 17095;
         }
         return montant;
     }
 
     getInvaliditeDeces(classe = 'C'): number {
-        if (this.accre && this._revenuPro() < 28962) {
-            return 0;
+        let montant = 0;
+        switch(classe) {
+            case 'A':
+                montant = 76;
+            break;
+            case 'B':
+                montant = 228;
+            break;
+            case 'C':
+                montant = 380;
+            break;
         }
-        return 380;
+
+        return montant;
     }
 
     getCsgCrds(): number {
@@ -142,15 +149,13 @@ export default class CotisationsSociales {
     }
 
     getCotisations(): number {
-        var total = 0;
-        total += this.getMaladie();
-        total += this.getAllocationsFamiliales();
-        total += this.getFormationProfessionnelle();
-        total += this.getRetraiteBase();
-        total += this.getRetraiteComplementaire();
-        total += this.getInvaliditeDeces();
-        total += this.getCsgCrds();
-        return total;
+        return this.getMaladie()
+            + this.getAllocationsFamiliales()
+            + this.getFormationProfessionnelle()
+            + this.getRetraiteBase()
+            + this.getRetraiteComplementaire()
+            + this.getInvaliditeDeces()
+            + this.getCsgCrds();
     }
 }
 
